3cc4937b9c2ae48fa2da2fb601a8ad46270ae824
grassi-giovanni <grassi.giovanni@iisgalvanimi.edu.it>
2021-11-09 00:03:18 +0000

    DOM

---
+++ script.js
@@ -0,0 +1,10 @@
+function button1Function() {
+    document.getElementById("mytext").innerHTML = "Hello World";
+    document.getElementById("testo2").innerHTML = "Sono il secondo testo";
+  }
+
+  function button2Function() {
+    document.getElementById("mytext").innerHTML = "Mi piace javascrpt";
+    document.getElementById("testo2").innerHTML = "A me invece non piace javascript";
+  }  
+  
